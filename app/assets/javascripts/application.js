d97d686d566aad8278311c1da5720fa0f4ecbb41
agnesarth <agnesarth@hotmail.fr>
2020-06-10 12:25:51 +0200

    jumbotron - home-page - create jumbotron with multiples images and bad transition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,17 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+//var images = new Array(
+//  'https://images.unsplash.com/photo-1558929996-da64ba858215?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80',
+//  'https://images.unsplash.com/photo-1541876176131-3f5e84a7331a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80',
+//  'https://images.unsplash.com/photo-1450778869180-41d0601e046e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1450&q=80',
+//  'https://images.unsplash.com/photo-1527416876370-fb74d128c3dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+//  'https://images.unsplash.com/photo-1532386236358-a33d8a9434e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1578&q=80',
+//);
+
+//var slider = setInterval(function() {
+//  document.getElementsByClassName('jumbotron-fluid')[0].setAttribute('style', 'background-image: url("'+images[0]+'")');
+//  images.splice(images.length, 0, images[0]);
+//  images.splice(0, 1);
+//}, 10000);
