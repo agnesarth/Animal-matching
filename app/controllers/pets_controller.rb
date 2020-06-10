e876aca79ecb4c56d1c2c6f48151a862b77a5cb5
mil3zh <milena.lazzaretti5@gmail.com>
2020-06-10 11:59:25 +0200

    upload

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,19 +2,16 @@ class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :destroy, :upload_photo, :delete_photo]
 
 
-  # GET /pets
-  # GET /pets.json
   def index
     @pets = Pet.all
   end
 
-  # GET /pets/new
+
   def new
     @pet = Pet.new
   end
 
-  # POST /pets
-  # POST /pets.json
+
   def create
     @pet = Pet.new(pet_params)
 
@@ -36,12 +33,10 @@ class PetsController < ApplicationController
     redirect_back(fallback_location: pet_path(@pet.id))
   end
 
-  # GET /pets/1
-  # GET /pets/1.json
+
   def show
   end
 
-  # GET /pets/1/edit
   def edit
     @pet = Pet.find(params[:id])
   end
@@ -52,8 +47,6 @@ class PetsController < ApplicationController
     redirect_back(fallback_location: pet_path(@pet.id))
   end
 
-  # PATCH/PUT /pets/1
-  # PATCH/PUT /pets/1.json
   def update
     respond_to do |format|
       if @pet.update(pet_params)
@@ -67,8 +60,6 @@ class PetsController < ApplicationController
     end
   end
 
-  # DELETE /pets/1
-  # DELETE /pets/1.json
   def destroy
     @pet.destroy
     respond_to do |format|
@@ -78,12 +69,10 @@ class PetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pet
       @pet = Pet.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def pet_params
       params.require(:pet).permit(:name, :animal, :chip_number, :breed, :sex, :age, photos: [])
     end
