c34c554061b7c4ccfbcc62aba98d79941d8f5cc1
lgentaz <lucile.gentaz@gmail.com>
2020-06-10 14:47:21 +0200

    like - controller - create and destroy likes + update match

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
 
-
-
     def already_liked(current_pet, other_pet)
         return current_pet.liked_likes.where(liker_id: other_pet).exists?
     end
@@ -11,6 +9,13 @@ class ApplicationController < ActionController::Base
         back_like.update(match: true)
         back_like.save
     end 
+
+    def unmatch(current_pet, other_pet)
+        iam_liked_ids = current_pet.liked_likes.all
+        unmatched = iam_liked_ids.where(liker: other_pet)
+        unmatched.match = false
+        unmatched.save
+    end
 
     def user_default_pet(current_user, my_pet)
         if current_user.default_pet_id.nil?
