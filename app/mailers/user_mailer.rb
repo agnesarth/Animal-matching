b86ea4c03e0afba3f029b1aae3a26cd4ed8c99f8
mil3zh <milena.lazzaretti5@gmail.com>
2020-06-10 18:53:27 +0200

    new pet email, changed text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,10 +13,10 @@ class UserMailer < ApplicationMailer
     mail(to: @pet.user.email, subject: 'Tu as ajouté un nouveau petit compagnon à quatre pattes')
   end
 
-  def new_match_email(user)
-    @user = user
+  def new_match_email(pet)
+    @pet = pet
     @url = 'whispaw.herokuapp.com'
-    mail(to: @user.email, subject: 'New match... Love is in the air!')
+    mail(to: @pet.user.email, subject: 'New match... Love is in the air!')
   end
 
 end
