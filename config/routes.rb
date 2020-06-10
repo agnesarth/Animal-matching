fc44af9adc2933087b11d2e65daaae36de8ca3d3
mil3zh <milena.lazzaretti5@gmail.com>
2020-06-10 10:40:39 +0200

    added destroy photo function to pet#edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pets do
     member do
-      delete "delete_photo/:photo_id", action: :delete_photo
+      delete :delete_photo
     end
   end
 end
