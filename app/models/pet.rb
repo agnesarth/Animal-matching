6c0b5b1b6d86cb9afcdaed21edfa340ae08697fb
lgentaz <lucile.gentaz@gmail.com>
2020-06-11 11:44:22 +0200

    tagpet - seed - seeded

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,13 +1,13 @@
 class Pet < ApplicationRecord
-  after_create :new_pet_send
-  after_update :new_match_send
+  #after_commit :new_pet_send
+  #after_update :new_match_send
 
   belongs_to :user
   has_many :likes_as_liker, foreign_key: "liker", class_name: "Like", dependent: :destroy
   has_many :likes_as_liked, foreign_key: "liked", class_name: "Like", dependent: :destroy
   has_many_attached :photos, dependent: :destroy
   has_many :tag_pets, dependent: :destroy
-  has_many :tags, through: :tag_whos
+  has_many :tags, through: :tag_pets
 
   BREED=['Terrier','Dalmatien','Manx','Birman','Boxer','Berger Allemand','Labrador','Bouledogue','Chihuahua','Beagle','Setter','Cocker','Husky','Teckel','Persan','Siamois','Somali','Sibérien','Ragdoll'].sort
 
