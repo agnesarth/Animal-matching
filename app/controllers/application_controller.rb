3cb74f327e02416f7d8b79769d07d40984f4e8e6
mil3zh <milena.lazzaretti5@gmail.com>
2020-06-12 11:20:21 +0200

    def redirect root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   include UsersHelper
   include PetsHelper
 
-  def after_sign_in_path_for(resource)
-    pets_path
-  end
+  #def after_sign_in_path_for(resource)
+    #pets_path
+  #end
 
 end
