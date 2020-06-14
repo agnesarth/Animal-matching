7cb4cd95ec46baf55c979390b8cab46d6af45b1e
Raphael-dln <delannay.raphael@gmail.com>
2020-06-14 17:09:10 +0200

    tag - model - change tag_chos to tag_pets

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   has_many :tag_pets, dependent: :destroy
-  has_many :pets, through: :tag_whos
+  has_many :pets, through: :tag_pets
   validates :value, uniqueness: true
 end
