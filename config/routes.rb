ba82dbeb218e714f75aed25b31919b25b67b66f2
Raphael-dln <delannay.raphael@gmail.com>
2020-06-14 15:44:20 +0200

    user - views - correct  user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   devise_for :users, path: "mon_compte"
-  resource :users, :except => [:new, :create, :index, :destroy], path: "mon_compte"
+  resource :users, :except => [:new, :create, :index, :destroy], path: "mon_profil"
   resources :pets, path: "animaux" do
     resources :likes
     member do
