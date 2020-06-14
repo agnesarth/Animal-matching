42a1c37456b89c32b051cac2032f4823ab2991d2
Raphael-dln <delannay.raphael@gmail.com>
2020-06-14 17:58:40 +0200

    tag - config - routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   devise_for :users, path: "mon_compte"
   resource :users, :except => [:new, :create, :index, :destroy], path: "mon_profil"
+ 
   resources :pets, path: "animaux" do
     resources :likes
+    resources :tags, :except => [:edit, :update]
     member do
       delete :delete_photo
     end
