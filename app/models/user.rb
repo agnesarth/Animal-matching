1af4130431921f8bd11fdc844f748935ba984398
jhon-ran <jhonnatan.rangel@gmail.com>
2020-06-15 09:44:05 +0200

    password validation - devise/user - moved password validation to devise from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,7 @@ class User < ApplicationRecord
     on: :update
 
   validates :email,
-    presence: true,
     uniqueness: true,
-    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Votre adresse email est erronée" }
 
   has_many :pets, dependent: :destroy
 
