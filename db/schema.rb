d9c2587b59dea1184a310d393b794fbf73925f03
agnesarth <agnesarth@hotmail.fr>
2020-06-17 01:17:24 +0200

    merge development into css_changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_202243) do
+ActiveRecord::Schema.define(version: 2020_06_16_212938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2020_06_16_202243) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.date "birthdate"
-    t.integer "age"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
