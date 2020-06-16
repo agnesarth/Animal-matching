9dd989ca5bdcadf15da46cd044881ed7dee50aca
agnesarth <agnesarth@hotmail.fr>
2020-06-16 22:41:02 +0200

    birthdate - migration - creating the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_170205) do
+ActiveRecord::Schema.define(version: 2020_06_16_202243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_06_13_170205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.datetime "birthdate"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
