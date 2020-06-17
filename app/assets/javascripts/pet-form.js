57f872ad0c1e3879440079720012b53048f9166c
mil3zh <milena.lazzaretti5@gmail.com>
2020-06-17 07:13:25 +0200

    assets - stylesheets - added icon on radio button once checked

--- app/assets/javascripts/pet-form.js
+++ app/assets/javascripts/pet-form.js
@@ -1,6 +1,10 @@
 function toggleRadioButton () {
-  var background = document.getElementById('radio-label')
-  console.log(background.classList)
-   //background.classList.remove('form-border')
-   background.classList.add('checked-form-border', this.checked)
+  $(this).find('label').addClass('checked-form-border')
+  console.log(radiobtn)
+  if (radiobtn.classList.contains('checked-form-border')) {
+    background.classList.remove('checked-form-border', this.unchecked)
+  } else {
+    background.classList.add('checked-form-border', this.checked)
+  }
+
 }
